fbc8a8a9746765ad7a8126ec1656975e7a5a2597
JoaquinRives <joaquin.rives01@gmail.com>
2019-10-06 21:21:14 +0300

    nothing

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -12,6 +12,13 @@ def test_health_endpoint_returns_200(flask_test_client):
     assert response.status_code == 200
 
 
+def test_index_health_200(flask_test_client):
+    
+    response = flask_test_client.get('/index', method='GET',
+        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
+    assert response.status_code == 200
+
+
 def test_actions_endpoint(flask_test_client):
 
     response = flask_test_client.get('/Relays/1/On', method='GET',
--- tests/test_index.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-def test_index_health_200(flask_test_client):
-    
-    response = flask_test_client.get('/index', method='GET',
-        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
-
-    assert response.status_code == 200
