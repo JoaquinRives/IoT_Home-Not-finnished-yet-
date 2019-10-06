1674b307a44893ba7938f6c5b3337b69a0746ac1
JoaquinRives <joaquin.rives01@gmail.com>
2019-10-06 19:30:51 +0300

    Added On/Off test

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,4 +1,8 @@
 import base64
+import RPi.GPIO as GPIO
+from app.raspberry import Raspberry_1
+
+rp1 = Raspberry_1()
 
 
 def test_health_endpoint_returns_200(flask_test_client):
@@ -8,53 +12,48 @@ def test_health_endpoint_returns_200(flask_test_client):
     assert response.status_code == 200
 
 
-def test_actions_endpoint_returns_200(flask_test_client):
+def test_actions_endpoint(flask_test_client):
 
     response = flask_test_client.get('/Relays/1/On', method='GET',
         headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
     assert response.status_code == 302
 
-    response = flask_test_client.get('/Relays/1/timer', method='GET',
-        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
-    assert response.status_code == 302
+    # Test if the relay 1 has turned "ON"
+    assert rp1.get_status(rp1.relay1)[0] == 'On'
 
     response = flask_test_client.get('/Relays/1/Off', method='GET',
         headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
     assert response.status_code == 302
 
-    response = flask_test_client.get('/Relays/1/Off', method='GET',
-        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
-    assert response.status_code == 302
-
-    response = flask_test_client.get('/Relays/2/auto', method='GET',
-        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
-    assert response.status_code == 302
+    # Test if the relay 1 has turned "OFF"
+    assert rp1.get_status(rp1.relay1)[0] == 'Off'
 
 
-def test_timer_endpoint_returns_200(flask_test_client):
-
-    response = flask_test_client.get('/timer/Relays/3', method='GET',
-        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
-    assert response.status_code == 200
-
-
-def test_auto_endpoint_returns_200(flask_test_client):
-
-    response = flask_test_client.get('/auto/Relays/2', method='GET',
+def test_set_auto_endpoint(flask_test_client): 
+    response = flask_test_client.get('/set_auto', method='GET',
         headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
     assert response.status_code == 302
 
 
-def test_set_timer_endpoint_returns_200(flask_test_client, method='GET',
-        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')}):  # TODO test POST form
+def test_set_timer_endpoint(flask_test_client):
+    response = flask_test_client.get('/set_timer', method='GET',
+        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
 
     response = flask_test_client.get('/set_timer', method='GET',
         headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
     assert response.status_code == 302
 
 
-def test_set_auto_endpoint_returns_200(flask_test_client):  # TODO test POST form
+def test_timer_endpoint(flask_test_client):
 
-    response = flask_test_client.get('/set_auto', method='GET',
+    response = flask_test_client.get('/timer/Relays/3', method='GET',
         headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
-    assert response.status_code == 302
+    assert response.status_code == 200
+
+
+def test_auto_endpoint(flask_test_client):
+
+    response = flask_test_client.get('/auto/Relays/2', method='GET',
+        headers={'Authorization': 'Basic ' + base64.b64encode(bytes('joaquin' + ":" + 'qwerty', 'ascii')).decode('ascii')})
+    assert response.status_code == 200
+
